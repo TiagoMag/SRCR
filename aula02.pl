0971c2df6091205ffe7926de707d2ccf9ec72477
TiagoMag <45245329+TiagoMag@users.noreply.github.com>
2020-02-21 16:44:27 +0000

    Add files via upload

--- aula02.pl
+++ aula02.pl
@@ -12,23 +12,23 @@
 :- set_prolog_flag( unknown,fail ).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado soma : n1,n2 -> {V,F}
+% Extensao do predicado soma : X,Y -> {V,F}
 
 soma1( X,Y ) :- G is X+Y,write(G).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado soma : n1,n2,n3 -> {V,F}
+% Extensao do predicado soma : X,Y,Z -> {V,F}
 
 soma( X,Y,Z ) :- G is X+Y+Z,write(G).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado soma : n1,n2,n3 -> {V,F}
+% Extensao do predicado soma : [A|B],Sum -> {V,F}
 
 soma([],0).
 soma([H|T],Sum) :- soma(T,N),Sum is H+N.
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado opart : n1,n2,n3 -> {V,F}
+% Extensao do predicado opart : X,Y,Op -> {V,F}
 
 opart( X,Y,Op ) :- (Op=='+') -> G is X+Y,write(G).
 opart( X,Y,Op ) :- (Op=='-') -> G is X-Y,write(G).
@@ -36,7 +36,7 @@ opart( X,Y,Op ) :- (Op=='*') -> G is X*Y,write(G).
 opart( X,Y,Op ) :- (Op=='/') -> G is X/Y,write(G).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado opL : n1,n2,n3 -> {V,F}
+% Extensao do predicado opL : [A|B],Acc,Op -> {V,F}
 
 
 opL([],1,'*').
@@ -45,31 +45,33 @@ opL([H|T],Acc,Op) :- (Op=='*') -> opL(T,N,Op), Acc is H*N.
 opL([H|T],Op) :- (Op=='+') -> soma([H|T],N).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado maior :  {V,F}
-
+% Extensao do predicado maior : X,Y ->  {V,F}
 
 maior(X,Y):- X>Y -> write(X).
 maior(X,Y):- Y>X -> write(Y).
+
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado maX : X,Y,Z ->  {V,F}
 
 max(X,Y,Z) :- (X>Y,X>Z) -> write(X).
 max(X,Y,Z) :- (Y>X,Y>Z) -> write(Y).
 max(X,Y,Z) :- (Z>X,Z>Y) -> write(Z).
 
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
+% Extensao do predicado maxL : [A|B],Max ->  {V,F}
+
 maxL([H],H).
 maxL([H|T],Max) :- maxL(T,Max),Max >= H.
 maxL([H|T],H) :- maxL(T,Max),H>Max.
 
-
-
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado maior :  {V,F}
-
+% Extensao do predicado media : [A|B],Avg ->  {V,F}
 
 average([H],H).
 average([H|T],Avg) :- soma([H|T],Sum),length([H|T],L),Avg is Sum/L.
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado maior :  {V,F}
+% Extensao do predicado isort : [A|B],S ->  {V,F}
 
 insert(X,[],[X]).
 insert(X,[H|T],[X,H|T]):- X=<H.
@@ -80,26 +82,25 @@ isort([H|T],S):-sort(T,S1),insert(H,S1,S).
 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado maior :  {V,F}
+% Extensao do predicado isortD : [A|B],S ->  {V,F}
 
 insertD(X,[],[X]).
 insertD(X,[H|T],[H,X|T]):- X>H.
 insertD(X,[H|T],[H|L]):- insert(X,T,L).
 
-sortD([],[]).
-sortD([H|T],S):-sort(T,S1),insert(H,S1,S).
+isortD([],[]).
+isortD([H|T],S):-isortD(T,S1),insert(H,S1,S).
 
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado maior :  {V,F}
+% Extensao do predicado countEmpty : [A|B],Sum ->  {V,F}
 
 
-nEmpty([],0).
-nEmpty([H|L],Sum) :- nEmpty(L,Res), H == [], Sum is Res + 1.
-nEmpty([H|L],Sum) :- nEmpty(L,Res), Sum is Res.
+countnEmpty([],0).
+countnEmpty([H|L],Sum) :- nEmpty(L,Res), H == [], Sum is Res + 1.
+countEmpty([H|L],Sum) :- nEmpty(L,Res), Sum is Res.
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
-% Extensao do predicado maior :  {V,F}
-
+% Extensao do predicado negacao : Predicado ->  {V,F}
 
 neg(X):- \+X.
